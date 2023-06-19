1c210d0b2d65b2bf910399909108109e1ad5703e
Agraw Mindaye <agrawmindaye@gmail.com>
2023-06-19 15:28:40 -0400

    user profile stuff

--- partials/nav.php
+++ partials/nav.php
@@ -1,4 +1,3 @@
-
 <?php
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
@@ -28,6 +27,7 @@ require_once(__DIR__ . "/../lib/functions.php");
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
