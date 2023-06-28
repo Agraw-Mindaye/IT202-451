6f311ed91f627d4330067d04cf74f495371c396c
Agraw Mindaye <agrawmindaye@gmail.com>
2023-06-28 16:43:31 -0400

    fixed login so user can login with  email or username

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -58,7 +58,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         //flash("Welcome, $email");
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email or username=email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email or username = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
